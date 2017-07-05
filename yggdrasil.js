4f91e6b3e11f9b5d3a58368b27c4073b24e98d6f
root <root@server.lan>
2017-07-05 22:40:34 +0100

    cards added

---
+++ yggdrasil.js
@@ -0,0 +1,9 @@
+//yggdrasil.js
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
