4deff450b6d58462f2ec2932e370100a4c61675c
Jeff Davies <hipparchus2000@gmail.com>
2017-07-16 19:12:48 +0100

    changed blog layout

--- yggdrasil.js
+++ yggdrasil.js
@@ -124,8 +124,8 @@ function applyProjectsTemplate() {
 			//hide buttons with undefined href and onclick
 			var allButtons = projectTemplatedInstance.getElementsByClassName("btn");
 			for (var i = 0; i < allButtons.length; i++) {
-				var hrefNotFound = allButtons[i].getAttribute("href")=="undefined";
-				var onclickNotFound = allButtons[i].getAttribute("onclick")=="undefined";
+				var hrefNotFound = allButtons[i].getAttribute("href").includes("undefined");
+				var onclickNotFound = allButtons[i].getAttribute("onclick").includes("undefined");
 				
 				if(hrefNotFound && onclickNotFound) {
 					var classAttribute = allButtons[i].getAttribute("class");
