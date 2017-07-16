7a1fca2b6c7fa66353ce506e8108449c8897a61a
Jeff Davies <hipparchus2000@gmail.com>
2017-07-16 14:41:56 +0100

    added start of templates and blog

--- yggdrasil.js
+++ yggdrasil.js
@@ -87,6 +87,35 @@ function getProjects() {
 return projects;
 }
 
+function registerServiceWorker () {
+	//register the service worker
+	if ('serviceWorker' in navigator) {
+	  window.addEventListener('load', function() {
+		navigator.serviceWorker.register('sw.js').then(function(registration) {
+		  // Registration was successful
+		  console.log('ServiceWorker registration successful with scope: ', registration.scope);
+		}).catch(function(err) {
+		  // registration failed :(
+		  console.log('ServiceWorker registration failed: ', err);
+		});
+	  });
+	}
+}
+
+function generateUUID(){
+    var d = new Date().getTime();
+    if(window.performance && typeof window.performance.now === "function"){
+        d += performance.now(); //use high-precision timer if available
+    }
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+
 
 var templates = document.getElementById("templates");
 if(templates!=null) {
