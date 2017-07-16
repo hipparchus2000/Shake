92ba21e96039ae0e9b3e8ded3d0bd2fd660103cc
Jeff Davies <hipparchus2000@gmail.com>
2017-07-16 18:12:35 +0100

    changed blog layout

--- yggdrasil.js
+++ yggdrasil.js
@@ -67,7 +67,7 @@ function getUrlAsHtmlUsingRest(url, callback) {
 	fetch(url).then(function(response) {
 	  return response;
 	}).then(function(data) {
-	  callback(data);
+	  callback(data.blob);
 	}).catch(function(err) {
 	  console.log("Failed To Get Url "+err);
 	});
