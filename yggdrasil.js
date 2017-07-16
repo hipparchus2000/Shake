9b2900e8f9f7de7c6b68e49767647a3029a32d07
Jeff Davies <hipparchus2000@gmail.com>
2017-07-16 17:22:15 +0100

    changed blog layout

--- yggdrasil.js
+++ yggdrasil.js
@@ -51,67 +51,118 @@ function generateUUID(){
 
 
 //REST fetch projects.json
-function getProjectsUsingRest(url, callback) {
+function getUrlUsingRest(url, callback) {
 	fetch(url).then(function(response) {
 	  return response.json();
 	}).then(function(data) {
 	  callback(data);
 	}).catch(function(err) {
-	  console.log("Failed To Get Projects "+err);
+	  console.log("Failed To Get Url "+err);
+	});
+}
+
+function applyProjectsTemplate() {
+	var templates = document.getElementById("templates");
+	var root = document.getElementById("root");
+	//clear all nodes from root
+	while (root.firstChild) {
+		root.removeChild(myNode.firstChild);
+	}
+
+	var cardrowTemplate = document.getElementById("cardRow-template");
+	var cardTemplate = document.getElementById("card-template");
+	
+	var projects = getUrlUsingRest("projects.json",function (response) {
+		projects = response;
+		
+		var rowcount=0;
+		var currentRow=null;
+		projects.forEach(function (project) {
+			if ((rowcount % 3) == 0) {
+				currentRow = cardrowTemplate.cloneNode(true);
+			}
+			var projectTemplatedInstance = cardTemplate.cloneNode(true);
+			
+			//if (project.codeUrl==null)
+			
+			projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{title}}/g, project.title);
+			projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{id}}/g, project.id);
+			projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{description}}/g, project.description);
+			projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{year}}/g, project.year);
+			projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{codeUrl}}/g, project.codeUrl);
+			projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{siteUrl}}/g, project.siteUrl);
+			projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{url}}/g, project.url);
+			projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{pdfUrl}}/g, project.pdfUrl);
+			
+			//hide buttons with "undefined" as href
+			var allButtons = projectTemplatedInstance.getElementsByClassName("btn");
+			for (var i = 0; i < allButtons.length; i++) {
+				if(allButtons[i].getAttribute("href")=="undefined") {
+					var classAttribute = allButtons[i].getAttribute("class");
+					allButtons[i].className = classAttribute+" hidden";
+				}
+			}
+
+			currentRow.append(projectTemplatedInstance);
+			if ((rowcount % 3) == 0) {
+				root.append(currentRow);
+			}
+			rowcount++;
+		});
+	});
+}
+
+
+function applyBlogTemplate() {
+	var templates = document.getElementById("templates");
+	var root = document.getElementById("root");
+	//clear all nodes from root
+	while (root.firstChild) {
+		root.removeChild(myNode.firstChild);
+	}
+
+	var blogTemplate = document.getElementById("blog-template");
+	
+	var projects = getUrlUsingRest("blog.json",function (response) {
+		blogEntries = response;
+		
+		blogEntries.forEach(function (story) {
+			var blogTemplatedInstance = blogTemplate.cloneNode(true);
+			blogTemplatedInstance.innerHTML = blogTemplatedInstance.innerHTML.replace(/{{storyText}}/g, story.storyText);
+			blogTemplatedInstance.innerHTML = blogTemplatedInstance.innerHTML.replace(/{{storyName}}/g, story.storyName);
+			blogTemplatedInstance.innerHTML = blogTemplatedInstance.innerHTML.replace(/{{date}}/g, story.date);
+			root.append(blogTemplatedInstance);
+		});
 	});
 }
 
 function refresh() {
-	var templates = document.getElementById("templates");
-	var root = document.getElementById("root");
+	
 	if(templates!=null) {
-		var cardrowTemplate = document.getElementById("cardRow-template");
-		var cardTemplate = document.getElementById("card-template");
 		
-		var projects = getProjectsUsingRest("projects.json",function (response) {
-			projects = response;
-			
-			var rowcount=0;
-			var currentRow=null;
-			projects.forEach(function (project) {
-				if ((rowcount % 3) == 0) {
-					currentRow = cardrowTemplate.cloneNode(true);
-				}
-				var projectTemplatedInstance = cardTemplate.cloneNode(true);
+		switch(route) {
+			case "/":
+				applyProjectsTemplate();
+				break;
 				
-				//if (project.codeUrl==null)
+			case "/blog":
+				applyBlogTemplate();
+				break;
 				
-				projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{title}}/g, project.title);
-				projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{id}}/g, project.id);
-				projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{description}}/g, project.description);
-				projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{year}}/g, project.year);
-				projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{codeUrl}}/g, project.codeUrl);
-				projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{siteUrl}}/g, project.siteUrl);
-				projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{url}}/g, project.url);
-				projectTemplatedInstance.innerHTML = projectTemplatedInstance.innerHTML.replace(/{{pdfUrl}}/g, project.pdfUrl);
-				
-				//hide buttons with "undefined" as href
-				var allButtons = projectTemplatedInstance.getElementsByClassName("btn");
-				for (var i = 0; i < allButtons.length; i++) {
-					if(allButtons[i].getAttribute("href")=="undefined") {
-						var classAttribute = allButtons[i].getAttribute("class");
-						allButtons[i].className = classAttribute+" hidden";
-					}
-				}
-
-				currentRow.append(projectTemplatedInstance);
-				if ((rowcount % 3) == 0) {
-					root.append(currentRow);
-				}
-				rowcount++;
-			});
-		});
+		} //end of templates switch
 		
 		
 	}
 }
 
+
+function navigate(newroute) {
+	route=newRoute;
+	refresh();
+}
+
 window.onload = function(){
+	var route="/";
 	refresh();
 }
 
