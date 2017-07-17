4797c9ee03787e6b821b1c06f28cc8d644b733cc
Jeff Davies <hipparchus2000@gmail.com>
2017-07-17 22:51:16 +0100

    dialog

--- yggdrasil.js
+++ yggdrasil.js
@@ -205,17 +205,15 @@ function navigate(newroute) {
 }
 
 window.onload = function(){
-	// When the user clicks on the button, open the modal 
+	var modal = document.getElementById('myModal');
+	var btn = document.getElementById("myBtn");
+	var span = document.getElementsByClassName("close")[0];
 	btn.onclick = function() {
 		modal.style.display = "block";
 	}
-
-	// When the user clicks on <span> (x), close the modal
 	span.onclick = function() {
 		modal.style.display = "none";
 	}
-
-	// When the user clicks anywhere outside of the modal, close it
 	window.onclick = function(event) {
 		if (event.target == modal) {
 			modal.style.display = "none";
@@ -225,13 +223,5 @@ window.onload = function(){
 	refresh();
 }
 
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 
