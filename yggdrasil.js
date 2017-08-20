27c127b9e75ade2f46307cbd8e0366a6b47dbf5b
Jeff Davies <hipparchus2000@gmail.com>
2017-08-20 22:04:07 +0100

    websockets test

--- yggdrasil.js
+++ yggdrasil.js
@@ -1028,7 +1028,7 @@ function fetchJwt() {
 }
 
 function initialise_websocketConnection() {
-	var serviceUrl = "ws://www.talkisbetter.com:58951";
+	var serviceUrl = "wss://www.talkisbetter.com:58951";
         var socket = new WebSocket(serviceUrl);
 
         socket.onopen = function () {
